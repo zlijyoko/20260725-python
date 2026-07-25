5916188cbf8a5cdf560a32303867dc842533ce78
zlijyoko <D1546781@o365.fcu.edu.tw>
2026-07-25 22:31:22 +0800

    homework1

---
+++ homework01.py
@@ -0,0 +1,27 @@
+# 建立三個變數，分別存放你的姓名、入學年份以及身高，並用print()個印出一行。
+
+Name = 'Yoko'
+Enrollment_year = 2026
+Height = 170
+
+print("My name is", Name)
+print("I enrolled in the year", Enrollment_year)
+print("My height is", Height, "cm.")
+
+
+# 已知 a = 8 , b = 2 ，請把兩個變數的值交換後印出。
+
+a = 8
+b = 2 
+c = a
+d = b
+
+print("a =", d, "b =", c)
+
+
+# 用 type() 列出下列各值的型別。 
+print(type( 42 ))
+print(type( 3.14 ))
+print(type( '42' ))
+print(type( True ))
+print(type( 10 /2 ))
