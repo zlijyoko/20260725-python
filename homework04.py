cf84751d49343fe8b9f08fcefa2a255984908b2f
zlijyoko <D1546781@o365.fcu.edu.tw>
2026-07-26 15:46:05 +0800

    hom4

--- homework04.py
+++ homework04.py
@@ -9,9 +9,17 @@ print("My name is ", name, ", I will be  ", (x + 1), " years old next year.")
 # 輸入圓半徑，輸出圓面積(取小數點後兩位，π 用 3.14159)。
 # 面積 = π * r²
 
-π = int(3.14159)
-r = input(" r = ")
-r2 = (f"{r} ^ {2} = {r**2}")
-area = (f"{π} * {r2} = {π*r2}")
-
+π = float( 3.14159 )
+r = input(" r : ")
+r = float( r )
+r2 = r*r
+r2 = float( r2 )
+area = (f"{3.14159} * {r2} = {3.14159*r2:.2f}")
 print('area = ',area)
+
+# 輸入攝氏溫度，輸出華氏溫度( 公式 : F = C * 9 / 5 + 32 )
+
+C = input('C: ')
+C = float( C )
+F = C * 9 / 5 + 32
+print( F )
